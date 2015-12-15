2dea6d4b50c55cab367d8704bc13685ccd9b7caf
Aaron Manaloto <aaronpomanaloto@gmail.com>
2015-12-16 00:11:23 +0800

    Fix request bug

--- songpls.rb
+++ songpls.rb
@@ -11,8 +11,8 @@ def get_service
   client = Google::APIClient.new(
     :key => DEVELOPER_KEY,
     :authorization => nil,
-    :application_name => $PROGRAM_NAME,
-    :application_version => '1.0.0'
+    :application_name => 'song-please',
+    :application_version => '0.0.0'
   )
   youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
@@ -46,7 +46,10 @@ get '/query' do
 
   text = "<https://youtu.be/#{videoId}|#{title}>"
 
-  response = RestClient.post response_url, {:text => text}
+  response =
+    RestClient.post response_url,
+      {:response_type => 'in_channel', :text => text}.to_json,
+      :content_type => :json
 
   status 200
   body ''
